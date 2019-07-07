69af8fdea2ba1e8485c6c903ae5c21dd58053354
Rafael Teixeira <rafael.amtx@gmail.com>
2019-07-07 15:57:13 -0300

    first commit

---
+++ src/main/java/blackwaterproject/blackwaterproject/BlackwaterprojectApplication.java
@@ -0,0 +1,13 @@
+package blackwaterproject.blackwaterproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackwaterprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackwaterprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/blackwaterproject/blackwaterproject/BlackwaterprojectApplicationTests.java
@@ -0,0 +1,16 @@
+package blackwaterproject.blackwaterproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlackwaterprojectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
